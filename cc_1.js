81595e1c132dd54d842407f245714b92f0897029
rileygoerg <rgoerg@usf.edu>
2024-05-15 13:59:01 -0400

    Create and test calcTip function.

--- cc_1.js
+++ cc_1.js
@@ -1,4 +1,13 @@
-let bill = 275;
-let tip = bill >= 50 && bill <= 300 ? bill * .15 : bill * .2;
-let total = tip+bill
-console.log("The bill was " + bill + ", the tip was " + tip + ", and the total was " + total + ".")
+//let bill = 275;
+//let tip = bill >= 50 && bill <= 300 ? bill * .15 : bill * .2;
+//let total = tip+bill;
+//console.log("The bill was " + bill + ", the tip was " + tip + ", and the total was " + total + ".");
+
+function calcTip(bill) {
+    return bill >= 50 && bill <= 300 ? bill * .15 : bill * .2;
+//calculates tip based on bill input
+}
+let testfunction = calcTip(100)
+//test variable with $100 bill
+console.log(testfunction)
+//displays tip from $100 bill
