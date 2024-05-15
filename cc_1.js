c491ddf9d79d0c32ee1bbed8f8e9b27530301d91
rileygoerg <rgoerg@usf.edu>
2024-05-15 13:48:58 -0400

    Add output functionality to display bill, tip, and total.

--- cc_1.js
+++ cc_1.js
@@ -1,2 +1,4 @@
 let bill = 275;
 let tip = bill >= 50 && bill <= 300 ? bill * .15 : bill * .2;
+let total = tip+bill
+console.log("The bill was " + bill + ", the tip was " + tip + ", and the total was " + total + ".")
