e1c7c86030a5f7518b2c2ce4e68b19084043388d
rileygoerg <rgoerg@usf.edu>
2024-05-15 14:30:35 -0400

    Implement and populate arrays for bills, tips, and totals.

--- cc_1.js
+++ cc_1.js
@@ -7,7 +7,18 @@ function calcTip(bill) {
     return bill >= 50 && bill <= 300 ? bill * .15 : bill * .2;
 //calculates tip based on bill input
 }
-let testfunction = calcTip(100)
-//test variable with $100 bill
-console.log(testfunction)
-//displays tip from $100 bill
+let bills = [[275,40,430],[125,555,44]];
+//matrix with both data sets
+let tips = [
+    [calcTip(bills[0][0]),calcTip(bills[0][1]),calcTip(bills[0][2])],
+    [calcTip(bills[1][0]),calcTip(bills[1][1]),calcTip(bills[1][2])]
+];
+//utilizing calcTip function to create respective tip matrix
+let totals = [
+    [bills[0][0]+tips[0][0],bills[0][1]+tips[0][1],bills[0][2]+tips[0][2]],
+    [bills[1][0]+tips[1][0],bills[1][1]+tips[1][1],bills[1][2]+tips[1][2]]
+];
+//summing each value in each respective matrix
+console.table(bills);
+console.table(tips);
+console.table(totals);
